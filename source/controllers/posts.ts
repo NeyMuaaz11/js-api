7240afb58b4f87f0f5764f5e529cd1ced761375b
m.rajput <m.rajput@sportradar.com>
2023-05-04 12:54:11 +0200

    directory cleanup

--- source/controllers/posts.ts
+++ source/controllers/posts.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import product from "../product.js";
 
 
-// TO-DO add getAllProducts, getProductsWithCategory
+// TO-DO add getAllProducts, getProductsWithCategory, deleteProducts
 
 
 
