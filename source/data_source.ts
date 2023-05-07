d37ad7c7a75157dd341bf012fa9900ac58ece847
= <=>
2023-05-07 20:36:56 +0200

    oopsie daisy

--- source/data_source.ts
+++ source/data_source.ts
@@ -8,9 +8,9 @@ const db = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
-    username: "root",
-    password: "123HeLLo456-",
-    database: "mysql",
+    username: "test",
+    password: "test",
+    database: "test",
     logging: true,
     synchronize: true,
     entities: [productModel]
